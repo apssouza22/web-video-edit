1a7ed3bcf76525d65ece3c195160636830077597
apssouza22 <apssouza22@gmail.com>
2025-10-02 19:11:14 +0100

    converted the constants

--- src/constants.ts
+++ src/constants.ts
@@ -7,15 +7,16 @@ export let fps = storedFps ? parseFloat(storedFps) : 24;
 export let max_size = 4000 * 1e6 / 4; // 4GB max
 
 export const AudioContext = window.AudioContext ||
+// @ts-ignore
     window.webkitAudioContext ||
     false;
 
 // Functions to update mutable globals
-export function setFps(newFps) {
-    fps = newFps;
-    localStorage.setItem('video-editor-fps', newFps.toString());
+export function setFps(newFps: number) {
+  fps = newFps;
+  localStorage.setItem('video-editor-fps', newFps.toString());
 }
 
-export function setMaxSize(newMaxSize) {
-    max_size = newMaxSize;
+export function setMaxSize(newMaxSize: number) {
+  max_size = newMaxSize;
 }
