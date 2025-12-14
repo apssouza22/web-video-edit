0f925cef61ead7657e272b9c8c97928741a15b4e
apssouza22 <apssouza22@gmail.com>
2025-12-14 15:23:02 +0000

    Removed multiple demux support (mp4box and html video)

--- src/video/demux/index.ts
+++ src/video/demux/index.ts
@@ -1,13 +1,10 @@
-import { CodecDemuxer } from './mp4boxdemuxer/codec-demuxer.js';
 import { MediaBunnyDemuxer } from './mediabunny-demuxer';
 import { VideoDemuxService } from './video-demux.js';
-import { HTMLVideoDemuxer } from './htmldemuxer/html-video-demuxer';
 
 export { VideoDemuxService } from './video-demux';
-export { WorkerVideoStreaming } from './worker-video-streaming';
 export type {VideoStreamingInterface} from "./types";
 
 export function createDemuxer(): VideoDemuxService {
-  return new VideoDemuxService(new MediaBunnyDemuxer(), new CodecDemuxer(), new HTMLVideoDemuxer());
+  return new VideoDemuxService(new MediaBunnyDemuxer());
 }
 
